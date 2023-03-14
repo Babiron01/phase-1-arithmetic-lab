5588cbf604384ce5823f4508cc22d6f4c19efc3c
Babikip01 <126768788+Babikip01@users.noreply.github.com>
2023-03-14 10:58:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 22;
+let num4 = 9;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10 ) + 1 ;
+let mod = num3 % num4 ;
+let max = Math.max(5,10,15,20);
 
-// Write your code here
+
+
+    
+    
+
+    
